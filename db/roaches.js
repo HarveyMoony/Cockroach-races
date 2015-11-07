aa7426b60b13d1eb8c7dacb370723af9a528c685
Harvey <harvey2009@yandex.ru>
2015-11-07 18:22:39 +0300

    first

---
+++ db/roaches.js
@@ -0,0 +1,42 @@
+var roaches = [
+    {
+        id: "r1",
+        name: "Micky",
+        speed: 2.99,
+        color: "#C0392B",
+        position: 0,
+        reaction: 20
+    },
+    {
+        id: "r2",
+        name: "Vasya",
+        speed: 2.98,
+        color: "#D35400",
+        position: 0,
+        reaction: 30
+    },
+    {
+        id: "r3",
+        name: "Petya",
+        speed: 3,
+        color: "#2980B9",
+        position: 0,
+        reaction: 25
+    },
+    {
+        id: "r4",
+        name: "Kolya",
+        speed: 2.96,
+        color: "#8E44AD",
+        position: 0,
+        reaction: 24
+    },
+    {
+        id: "r5",
+        name: "Olya",
+        speed: 2.97,
+        color: "#2C3E50",
+        position: 0,
+        reaction: 26
+    }
+];
